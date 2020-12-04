201da6c4e2dc41917116c09c0b25848aa88ddfda
Paul Watson <paul.watson@pdwtech.com>
2020-12-04 08:16:33 +0000

    Day04 part02

--- day04/main.py
+++ day04/main.py
@@ -1,20 +1,28 @@
+import re
 from functools import reduce
-
-fields = ['byr', 'iyr', 'eyr', 'hgt', 'hcl', 'ecl', 'pid']
 
 
 def part01(input):
     passport_strings = input.split("\n\n")
-
     passports = [to_passport(lines) for lines in passport_strings]
-    # print(passports)
+    return sum(1 for p in passports if required_keys(p))
 
+
+def part02(input):
+    passport_strings = input.split("\n\n")
+    passports = [to_passport(lines) for lines in passport_strings]
     return sum(1 for p in passports if valid(p))
 
 
 def valid(passport):
     keys = passport.keys()
-    return all(elem in keys for elem in fields)
+    keys_to_validate = list(set(passport.keys()) & set(validation.keys()))
+    need_validation = {k: passport[k] for k in keys_to_validate}
+    return required_keys(keys) and all([validation[k](v) for k, v in need_validation.items()])
+
+
+def required_keys(keys):
+    return all(elem in keys for elem in validation.keys())
 
 
 def combine_dicts(acc, line):
@@ -22,7 +30,6 @@ def combine_dicts(acc, line):
 
 
 def to_passport(lines):
-    # z = x | y
     return reduce(combine_dicts, lines.splitlines(), {})
 
 
@@ -34,3 +41,50 @@ def parse_line(line):
 def to_key_value(keyvalue_str):
     kv = keyvalue_str.split(':')
     return kv[0], kv[1]
+
+
+def byr_valid(value):
+    return int(value) in range(1920, 2003)
+
+
+def iyr_valid(value):
+    return int(value) in range(2010, 2021)
+
+
+def eyr_valid(value):
+    return int(value) in range(2020, 2031)
+
+
+def hgt_valid(value):
+    match = re.search(r'(\d+)(cm|in)', value)
+    if match:
+        height = int(match.group(1))
+        if match.group(2) == 'in':
+            return height in range(59, 77)
+        else:
+            return height in range(150, 194)
+    else:
+        return False
+
+
+def hcl_valid(value):
+    return bool(re.match(r'#[0-9a-f]{6}', value))
+
+
+def pid_valid(value):
+    return bool(re.match(r'\d{9}$', value))
+
+
+def ecl_valid(value):
+    return value in ['amb', 'blu', 'brn', 'gry', 'grn', 'hzl', 'oth']
+
+
+validation = {
+    'byr': byr_valid,
+    'iyr': iyr_valid,
+    'eyr': eyr_valid,
+    'hgt': hgt_valid,
+    'hcl': hcl_valid,
+    'ecl': ecl_valid,
+    'pid': pid_valid
+}
--- day04/test_main.py
+++ day04/test_main.py
@@ -1,7 +1,8 @@
 import textwrap
 from unittest import TestCase
 
-from main import part01, to_key_value, parse_line, to_passport, valid
+from main import part01, to_key_value, parse_line, to_passport, valid, byr_valid, hgt_valid, hcl_valid, pid_valid, \
+    ecl_valid, iyr_valid, part02
 
 
 def get_input(name):
@@ -13,13 +14,17 @@ class Test(TestCase):
     def test_part01(self):
         input = get_input('input.txt')
         print(part01(input))
-        
+
+    def test_part02(self):
+        input = get_input('input.txt')
+        print(part02(input))
+
     def test_testpart01(self):
         input = get_input('testinput.txt')
         assert part01(input) == 2
 
     def test_kv(self):
-        assert to_key_value('ecl:gry') == ('ecl', 'grygry')
+        assert to_key_value('ecl:gry') == ('ecl', 'gry')
         assert to_key_value('pid:860033327') == ('pid', '860033327')
 
     def test_parse_values(self):
@@ -44,4 +49,42 @@ byr:1937 iyr:2017 cid:147 hgt:183cm"""
         assert valid(testinput[1]) == False
         assert valid(testinput[2]) == True
         assert valid(testinput[3]) == False
-        
+
+    def test_byr_valid(self):
+        assert byr_valid('1919') == False
+        assert byr_valid('1920') == True
+        assert byr_valid('2002') == True
+        assert byr_valid('2003') == False
+
+    def test_hgt_valid(self):
+        # assert hgt_valid('123in') == False
+        assert hgt_valid('58in') == False
+        assert hgt_valid('59in') == True
+        assert hgt_valid('76in') == True
+        assert hgt_valid('74in') == True
+        assert hgt_valid('77in') == False
+
+        assert hgt_valid('149cm') == False
+        assert hgt_valid('150cm') == True
+        assert hgt_valid('193cm') == True
+        assert hgt_valid('194cm') == False
+
+    def test_hcl_valid(self):
+        assert hcl_valid('#111fff') == True
+        assert hcl_valid('#111ffg') == False
+        assert hcl_valid('#111ff') == False
+
+    def test_pid_valid(self):
+        assert pid_valid('000000001') == True
+        assert pid_valid('0123456789') == False
+
+    def test_ecl_valid(self):
+        assert ecl_valid('amb') == True
+        assert ecl_valid('blu') == True
+        assert ecl_valid('asdf') == False
+
+    def test_valid(self):
+        input = to_passport("pid:087499704 hgt:74in ecl:grn iyr:2012 eyr:2030 byr:1980 hcl:#623a2f")
+        assert valid(input) == True
+        input = to_passport("eyr:2029 ecl:blu cid:129 byr:1989 iyr:2014 pid:896056539 hcl:#a97842 hgt:165cm")
+        assert valid(input) == True
