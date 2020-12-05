5d0416efb17d29c2ce1e455e2713eea643ab1db0
Paul Watson <paul.watson@pdwtech.com>
2020-12-05 06:21:24 +0000

    Day05 Groovy

---
+++ day05/day05.groovy
@@ -0,0 +1,38 @@
+def readFromFile(file) {
+    (file as File).readLines().collect()
+}
+
+def binarySearch(instructions, seats, lowerChar ) {
+    def n = seats.size /2
+    if (seats.size == 1) {
+        return seats[0]
+    } else if (instructions[0] == lowerChar) {
+        return binarySearch(instructions.drop(1), seats[0..n-1], lowerChar)
+    } else {
+        return binarySearch(instructions.drop(1), seats[n..seats.size-1], lowerChar)
+    }
+}
+
+assert binarySearch('FBFBBFFRLR', (0..127).collect(), 'F') == 44
+assert binarySearch('RLR', (0..7).collect(), 'L') == 5
+
+def calcSeatNumber(seat) {
+    def row = binarySearch(seat.take(7), (0..127).collect(), 'F')
+    def col = binarySearch(seat.drop(7), (0..7).collect(), 'L')
+    return row * 8 + col
+}
+
+assert calcSeatNumber('FBFBBFFRLR') == 357
+
+def boardingPasses = readFromFile('input.txt')
+def seatNumbers = boardingPasses.collect { calcSeatNumber(it) }
+
+def maxSeatNumber = seatNumbers.max()
+println "part01: $maxSeatNumber"
+def sortedSeatNumbers = seatNumbers.sort()
+
+
+for (n in 0..<sortedSeatNumbers.size-1) {
+    (a,b) = sortedSeatNumbers[n..n+1]
+    if (b-a != 1) println "Part02: ${b-1}"
+}
