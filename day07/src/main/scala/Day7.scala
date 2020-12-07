caafe5f0479deb348658840fc7fdd0605e2cfbd9
paulwatson <@w4tson>
2020-12-07 14:31:44 +0000

    Tidy

--- day07/src/main/scala/Day7.scala
+++ day07/src/main/scala/Day7.scala
@@ -1,38 +1,26 @@
 import scala.io.Source
-import scala.util.matching.Regex
 
-class DayX(input: String) {
+class Day7(input: String) {
   
   def solve(): Unit ={
-//    val fileContents = Source.fromFile(input).getLines.mkString
     val graph = new Graph()
     val lines = Source.fromFile(input).getLines.toList
 
     lines.foreach( line => parseLine(line, graph))
-//    println(graph)
-    graph.part01(graph.getVertex("shiny gold"))
+    val shinyGold = graph.getVertex("shiny gold")
+    graph.part01(shinyGold)
+    graph.part02(shinyGold)
   }
-
-
-
-
-//  muted yellow bags contain 2 shiny gold bags, 9 faded blue bags.
 
   def parseLine(line: String, graph: Graph ) = {
     val pattern = "(\\w+ \\w+) bags contain (no other bags|.*).$".r
     for (m <- pattern.findAllMatchIn(line)) {
-//      graph.add(new Vertex(m.group(1)))
-
       if (!m.group(2).startsWith("no other")) {
         parseBagInfo(m.group(2)).foreach { n =>
-          print(n)
           graph.addEdge(m.group(1), n._2, n._1)
         }
       }
-
-      println(s"${m.group(1)} ${m.group(2)}")
     }
-
   }
 
   def parseBagInfo(bagsInfo: String): List[(Int, String)] = {
@@ -42,13 +30,23 @@ class DayX(input: String) {
         (m.group(1).toInt, m.group(2))
       }.orNull
     }).toList
-
   }
 }
 
 class Graph() {
   var vertices : List[Vertex] = List()
   var edges : List[Edge] = List()
+
+  def part01(vertex: Vertex) = {
+    val visited = Map(vertex -> true)
+    val paths = dfs(getVertex(vertex.color), visited, List())
+    println(paths.flatMap{ v => v.collect{ (x: Vertex) => x.color} }.distinct.size-1)
+  }
+
+  def part02(vertex: Vertex) = {
+    val count = bfs(getVertex(vertex.color), 1) -1
+    println(count)
+  }
 
   def addEdge(from: String, to: String, count: Int) = {
     this.edges = new Edge(getVertex(from), getVertex(to), count)::this.edges
@@ -76,27 +74,35 @@ class Graph() {
       .collect{ e => e.from }.toList
   }
 
-  def part01(vertex: Vertex) = {
-    val visited = Map(vertex -> true)
-    val paths = dfs(getVertex(vertex.color), visited, List(), getAllParents)
-    println(paths.flatMap{ v => v.collect{ (x: Vertex) => x.color} }.distinct.size-1)
+  def getAllChildren(color: String) : List[Edge] = {
+    this.edges.filter{e => e.from.color == color }
   }
 
-  def dfs(current: Vertex, visited: Map[Vertex, Boolean], paths: List[List[Vertex]], getAdjacentNodes: String => List[Vertex]):  List[List[Vertex]] = {
+  def dfs(current: Vertex, visited: Map[Vertex, Boolean], paths: List[List[Vertex]]):  List[List[Vertex]] = {
     val visited_updated = visited + (current -> true)
-    val parents = getAdjacentNodes(current.color)
+    val parents = getAllParents(current.color)
     var result = paths
 
     parents.foreach{ parent =>
       val has_visited: Boolean = visited_updated.getOrElse(parent, false)
       if (!has_visited) {
-        result = result ++ dfs(parent, visited_updated, paths, getAllParents)
+        result = result ++ dfs(parent, visited_updated, paths)
       }
     }
 
     parents.size match {
       case 0 => visited_updated.keys.toList::result
       case _ => result
+    }
+  }
+
+  def bfs(current: Vertex, multiplier: Int):  Int = {
+    val children = getAllChildren(current.color)
+    children.size match {
+      case 0 => multiplier
+      case _ => multiplier + children.map(child => {
+        multiplier * bfs(child.to, child.count)
+      }).sum
     }
   }
 
