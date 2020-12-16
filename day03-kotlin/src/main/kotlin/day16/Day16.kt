ba761145cdb30f2ed2dc2023f632f71d1d366870
paulwatson <@w4tson>
2020-12-16 17:45:52 +0000

    wip

--- day03-kotlin/src/main/kotlin/day16/Day16.kt
+++ day03-kotlin/src/main/kotlin/day16/Day16.kt
@@ -2,6 +2,7 @@ package day16
 
 data class Rule(val name: String, val range1: IntRange, val range2 : IntRange) {
     fun matches(value: Int) : Boolean = (value in range1) or (value in range2)
+    fun matches(t: Ticket) : Boolean = t.values.all(this::matches)
 }
 
 
@@ -12,13 +13,54 @@ data class Ticket(val values: List<Int>) {}
 
 class Day16(val rules: List<Rule>){
 
+    var position : MutableMap<Rule, Int> = mutableMapOf()
+
     fun errorRates(tickets : List<Ticket>) : Int = tickets.sumBy { errorRate(it) }
+    fun validTickets(tickets : List<Ticket>) : List<Ticket> = tickets.filter { valid(it) }
 
     fun errorRate(ticket : Ticket) : Int {
         return ticket.values.filter { matchingRules(it).isEmpty() }.sum()
     }
 
+    fun valid(ticket: Ticket) : Boolean {
+        return ticket.values.all { matchingRules(it).isNotEmpty() }
+    }
+
     fun matchingRules(value: Int) : List<Rule> = rules.filter { it.matches(value) }
+
+
+
+    fun part2(tickets : List<Ticket>) : Int{
+        val validTickets = validTickets(tickets)
+
+        (validTickets[0].values.indices).map { i ->
+            validTickets.map { t ->
+                rules.count { it.matches(t.values[i]) }
+            }.min()
+        }.forEach { println(it) }
+
+//        val min = validTickets.map { t ->
+//            rules.count { it.matches(t.values[i]) }
+//        }.forEach { println(it) }
+//        println(min)
+
+
+        return -1
+
+    }
+
+    fun foo(index: Int, tickets: List<Ticket>) : Pair<Int, Ticket> {
+        when (tickets.size) {
+            1 -> return Pair(index, tickets.first())
+            else -> {
+                (validTickets[0].values.indices).map { i ->
+                    validTickets.map { t ->
+                        rules.count { it.matches(t.values[i]) }
+                    }.min()
+                }.forEach { println(it) }
+            }
+        }
+    }
 
 }
 
@@ -26,7 +68,7 @@ class Day16(val rules: List<Rule>){
 val r = Regex("([\\w ]+): (\\d+)-(\\d+) or (\\d+)-(\\d+)")
 
 fun toRule(line: String) : Rule {
-    println(line)
+//    println(line)
     val match = r.find(line)!!
     val (name, from1, to1, from2, to2) = match.destructured
     return Rule(name, (from1.toInt()..to1.toInt()), (from2.toInt()..to2.toInt()))
--- day03-kotlin/src/test/kotlin/day16/Day16Test.kt
+++ day03-kotlin/src/test/kotlin/day16/Day16Test.kt
@@ -6,18 +6,18 @@ import org.junit.Test
 class Day16Test {
 
     @Test
-    fun name() {
+    fun part01() {
         val input = readInput("/day16/day16.txt")
-        val foo = input.split("\n\n")
-        val rules = foo[0].lines().map { toRule(it) }
-        val tickets = foo[2].lines()
+        val parts = input.split("\n\n")
+        val rules = parts[0].lines().map { toRule(it) }
+        val tickets = parts[2].lines()
             .drop(1)
             .map { it.split(",").map(String::toInt).toList() }
             .map{ Ticket(it) }
-        println(rules)
-//        assert(3 in rules[0].range1)
-        println(tickets)
-        val errorRates = Day16(rules).errorRates(tickets)
-        println(errorRates)
+        val day16 = Day16(rules)
+//        val errorRates = day16.errorRates(tickets)
+//        println(errorRates)
+
+        day16.part2(tickets)
     }
 }
