80301df1f7b294817f00e7291bb80226a905f35e
Paul Watson <paul.watson@pdwtech.com>
2020-12-20 21:19:53 +0000

    Day18

--- day03-kotlin/src/main/kotlin/day18/Parser.kt
+++ day03-kotlin/src/main/kotlin/day18/Parser.kt
@@ -2,58 +2,53 @@ package day18
 
 import java.util.*
 
-sealed class Expr
-data class Const(var value: Long) : Expr()
-data class Sum(var left: Expr, var right: Expr) : Expr()
-data class Mul(var left: Expr, var right: Expr) : Expr()
+fun evalPostfixx(expr : String) : Long {
+    val stack : Stack<Long> = Stack()
 
-fun eval(expr: Expr): Long = when(expr) {
-    is Const -> expr.value
-    is Sum -> eval(expr.left) + eval(expr.right)
-    is Mul -> eval(expr.left) * eval(expr.right)
+    expr.toCharArray().forEach { c -> 
+        when (c) {
+            in '0'..'9' -> stack.push(Character.getNumericValue(c).toLong())
+            '*' -> {
+                stack.push(stack.pop() * stack.pop())
+            }
+            '+' -> {
+                stack.push(stack.pop() + stack.pop())
+            }
+        }
+    }
+    
+    return stack.pop()
 }
 
-fun Char.isOperand() : Boolean = this == '+' || this == '*'
+class Parser(val operatorPrecedence : Map<Char, Int>) {
+    
+    fun exec(expression: String) : String {
+        var postfixList = ""
+        val opStack = Stack<Char>()
 
-class Parser(val expression: String) {
-    var s : Stack<Char> = Stack()
-    var e : Stack<Expr> = Stack()
-    var tmp: String = ""
-
-    operator fun invoke(): Expr {
-        expression.filter { it != ' ' }.forEach {
-            // consts to e stack
-            //
-            if (tmp == "" && it != '(') {
-                if (it.isDigit()) {
-                    e.push(Const(Character.getNumericValue(it).toLong()))
-                } else {
-                    s.push(it)
+        expression.filter { it != ' ' }.forEach { c ->
+            if (Character.getNumericValue(c) in 0..9) {
+                postfixList += c
+            } else if (c == '(') {
+                opStack.push(c)
+            } else if (c == ')') {
+                var topToken = if (opStack.empty()) '(' else opStack.pop()
+                while (topToken != '(' && opStack.isNotEmpty() && operatorPrecedence[opStack.peek()]!! >= operatorPrecedence[c]!! ) {
+                    postfixList += topToken
+                    topToken = if (opStack.empty()) '(' else opStack.pop() 
                 }
-
             } else {
-                if (it == ')') {
-                    e.push(Parser(tmp)())
-                    tmp = ""
-                } else if (it == '(') {
-                    tmp = " "
-                } else {
-                    tmp += it
+                while (opStack.isNotEmpty() && opStack.isNotEmpty() && operatorPrecedence[opStack.peek()]!! >= operatorPrecedence[c]!! ) {
+                    postfixList += opStack.pop()
                 }
-            }
-
-            if (tmp.isEmpty() && s.isNotEmpty() && s.peek().isOperand() && e.size >= 2) {
-                val right = e.pop()
-                val left = e.pop()
-                e.push(
-                    when (s.pop()) {
-                        '+' -> Sum(left, right)
-                        else -> Mul(left, right)
-                    }
-                )
+                opStack.push(c)
             }
         }
 
-        return e.pop()
+        while (!opStack.isEmpty()) {
+            postfixList += opStack.pop()
+        }
+        
+        return postfixList
     }
 }
--- day03-kotlin/src/test/kotlin/day18/Day18Test.kt
+++ day03-kotlin/src/test/kotlin/day18/Day18Test.kt
@@ -5,28 +5,40 @@ import org.junit.Assert.*
 import org.junit.Test
 
 class Day18Test {
-    
+
+    val input = readInput("/day18/day18.txt")
+
+
+    private val operatorPrecedence1 = mapOf(
+        Pair('*', 2),
+        Pair('+', 2),
+        Pair('(', 1),
+        Pair(')', 1)
+    )
+
+    private val operatorPrecedence2 = mapOf(
+        Pair('*', 2),
+        Pair('+', 3),
+        Pair('(', 1),
+        Pair(')', 1)
+    )
+
     @Test
-    fun testBinaryTree() {
-        val str = readInput("/day18/day18test.txt")
-        println(str)
-//        Parser(str).parse()
-        
-        //1 + (2 * 3) + (4 * (5 + 6))
-        val left = Sum(Const(1), Mul(Const(2), Const(3)))
-        val right = Mul(Const(4), Sum(Const(5), Const(6)))
-        assertEquals(51, eval(Sum(left, right)))
+    fun part1() {
+        val parser = Parser(operatorPrecedence1)
+        assertEquals(12240, evalPostfixx(parser.exec("5 * 9 * (7 * 3 * 3 + 9 * 3 + (8 + 6 * 4))")))
+        assertEquals(13632, evalPostfixx(parser.exec("((2 + 4 * 9) * (6 + 9 * 8 + 6) + 6) + 2 + 4 * 2")))
+
+        val result = input.lines().map { evalPostfixx(parser.exec(it)) }.sum()
+        println(result)
     }
 
     @Test
-    fun name() {
-        val str = readInput("/day18/day18test.txt")
-//        println(Parser("1+1")())
-        assertEquals(10, eval(Parser("5*(1+1)")()))
-        assertEquals(26, eval(Parser("2*3+(4*5)")()))
-        assertEquals(13632, eval(Parser("((2 + 4 * 9) * (6 + 9 * 8 + 6) + 6) + 2 + 4 * 2")()))
-        val result = str.lines().map { eval(Parser(it)()) }.sum()
-        println(result)
+    fun part2() {
+        val parser = Parser(operatorPrecedence2)
+        assertEquals(669060, evalPostfixx(parser.exec("5 * 9 * (7 * 3 * 3 + 9 * 3 + (8 + 6 * 4))")))
 
+        val result = input.lines().map { evalPostfixx(parser.exec(it)) }.sum()
+        println(result)
     }
 }
