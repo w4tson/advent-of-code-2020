9e043ccbf8f2559f525f1017b7b304795a1a3853
Paul Watson <paul.watson@pdwtech.com>
2020-12-22 22:22:10 +0000

    Day19 part 2

--- day03-kotlin/src/main/kotlin/day19/Day19.kt
+++ day03-kotlin/src/main/kotlin/day19/Day19.kt
@@ -1,5 +1,7 @@
 package day19
 
+import java.lang.IllegalStateException
+
 
 data class RuleMatch(val index1: Int, val index2: Int)
 
@@ -8,28 +10,56 @@ sealed class Rule(val index: Int)
 data class Value(val id: Int, val value : String) : Rule(id) 
 data class EitherMatch(val id: Int, val r1 : RuleMatch, val r2: RuleMatch) : Rule(id)
 data class SingleEitherMatch(val id: Int, val v1 : Int, val v2: Int) : Rule(id)
+
+data class TripeMatch(val id: Int, val r1 : RuleMatch, val index3: Int) : Rule(id)
 data class DoubleMatch(val id: Int, val r1 : RuleMatch) : Rule(id)
 data class SingleMatch(val id: Int, val v1: Int)  : Rule(id)
 
+typealias Rules = Map<Int, Rule>
 
+fun matches(s : String, rules: Rules) : Boolean {
+    val m = _matches(s, 0, rules, 0)
+    return m.second && s.length == m.first 
+} 
 
-fun buildRegex(rules : Map<Int, Rule>) : String {
-    return _buildRegex(0, rules, "")
-}
-
-fun _buildRegex(i: Int, rules : Map<Int, Rule>, acc : String) : String {
-    return when (val rule = rules.get(i)!!) {
-        is Value -> rule.value
-        is DoubleMatch -> acc + _buildRegex(rule.r1.index1, rules, acc) + _buildRegex(rule.r1.index2, rules, acc)
-        is SingleMatch -> acc + _buildRegex(rule.v1, rules, acc)
+fun _matches(s : String, ruleId: Int, rules : Map<Int, Rule>, index: Int) : Pair<Int, Boolean> {
+    val rule = rules.get(ruleId)
+    val res : Pair<Int, Boolean> = when (rule) {
+        is Value -> Pair(index+1, s[index] == rule.value[0])
+        is SingleMatch -> _matches(s, rule.v1, rules, index)
+        is DoubleMatch -> {
+                listOf(rule.r1.index1, rule.r1.index2).fold(Pair(index, true), { acc, rId ->
+                    val (newIndex, bool) = _matches(s, rId, rules, acc.first)
+                    Pair(newIndex, acc.second && bool)
+                })
+        }
+        is TripeMatch -> {
+            listOf(rule.r1.index1, rule.r1.index2, rule.index3).fold(Pair(index, true), { acc, rId ->
+                val (newIndex, bool) = _matches(s, rId, rules, acc.first)
+                Pair(newIndex, acc.second && bool)
+            })
+        }
         is SingleEitherMatch -> {
-            acc + "(" + _buildRegex(rule.v1, rules, acc) + "|" + _buildRegex(rule.v2, rules, acc) + ")"
+            val m1 = _matches(s, rule.v1, rules, index)
+            if (m1.second) m1 else {
+                _matches(s, rule.v2, rules, index)    
+            }
         }
         is EitherMatch -> {
-            acc + "(" + _buildRegex(rule.r1.index1, rules, acc) + _buildRegex(rule.r1.index2, rules, acc) + 
-                    "|" +
-                        _buildRegex(rule.r2.index1, rules, acc) + _buildRegex(rule.r2.index2, rules, acc) +
-                    ")"
+            val m1 = listOf(rule.r1.index1, rule.r1.index2).fold(Pair(index, true), { acc, rId ->
+                val (newIndex, bool) = _matches(s, rId, rules, acc.first)
+                Pair(newIndex, acc.second && bool)
+            })
+            if (m1.second) m1 else {
+                listOf(rule.r2.index1, rule.r2.index2).fold(Pair(index, true), { acc, rId ->
+                    val (newIndex, bool) = _matches(s, rId, rules, acc.first)
+                    Pair(newIndex, acc.second && bool)
+                }) 
+            }
         }
+        else -> throw IllegalStateException("Nope")
     }
+    return res
+    
 }
+
--- day03-kotlin/src/test/kotlin/day19/Day19Test.kt
+++ day03-kotlin/src/test/kotlin/day19/Day19Test.kt
@@ -2,16 +2,21 @@ package day19
 
 import Util.Companion.readInput
 import org.junit.Test
+import kotlin.test.assertFalse
+import kotlin.test.assertTrue
 
 class Day19Test {
     
     val r = Regex("^(\\d+): (\\d+) (\\d+)(?: \\| (\\d+) (\\d+))?$")
     val singleEther = Regex("^(\\d+): (\\d+) \\| (\\d+)$")
+    val triple = Regex("^(\\d+): (\\d+) (\\d+) (\\d+)$")
     val singleVal = Regex("(\\d+): \"(\\w+)\"")
     val singleIndex = Regex("^(\\d+): (\\d+)$")
 
+    private fun buildRules(ruleStr: String): Rules = ruleStr.lines().map { toRule(it) }.associateBy { it.index }
 
     fun toRule(s: String) : Rule {
+//        println(s)
         if (s.contains("\"")) {
             val (id, value) = singleVal.find(s)!!.destructured
             return Value(id.toInt(), value)
@@ -21,6 +26,9 @@ class Day19Test {
         } else if (singleEther.matches(s)) {
             val (index, rule1, rule2) = singleEther.find(s)!!.destructured
             return SingleEitherMatch(index.toInt(), rule1.toInt(), rule2.toInt())
+        } else if (triple.matches(s)) {
+            val (index, rule1, rule2, rule3) = triple.find(s)!!.destructured
+            return TripeMatch(index.toInt(), RuleMatch(rule1.toInt(), rule2.toInt()), rule3.toInt())
         } else  {
             val match = r.find(s)!!
             val (index, rule1, rule2, rule3, rule4) = match.destructured
@@ -32,13 +40,33 @@ class Day19Test {
             }
         }
     }
-    
+
+    @Test
+    fun part1example() {
+        val (ruleStr, input) = readInput("/day19/test.txt").split("\n\n")
+        val rules = buildRules(ruleStr)
+        val lines = input.lines()
+        assertTrue(matches(lines[0],  rules))
+        assertFalse(matches(lines[1], rules))
+        assertTrue(matches(lines[2],  rules))
+        assertFalse(matches(lines[3], rules))
+        assertFalse(matches(lines[4], rules))
+    }
+
     @Test
     fun part1() {
         val (ruleStr, input) = readInput("/day19/day19.txt").split("\n\n")
-        val rules = ruleStr.lines().map { toRule(it) }.associateBy { it.index }
-        val reg = Regex(buildRegex(rules))
-        val count = input.lines().count { reg.matches(it) }
+        val rules = buildRules(ruleStr)
+        val count = input.lines().count { matches(it,rules) }
         println(count)
+    }
+
+    @Test
+    fun part2() {
+        val (ruleStr, input) = readInput("/day19/day19.txt").split("\n\n")
+//        val rules = buildRules(ruleStr.replace("8: 42", "8: 42 | 42 8").replace("11: 42 31", ))
+        
+//        println(count)
+//        8: 42
     }
 }
