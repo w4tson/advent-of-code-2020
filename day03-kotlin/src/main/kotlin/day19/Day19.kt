aac1c6d46afd297a7bea7c77c323c82220218f52
Paul Watson <paul.watson@pdwtech.com>
2020-12-22 22:31:59 +0000

    refactoring

--- day03-kotlin/src/main/kotlin/day19/Day19.kt
+++ day03-kotlin/src/main/kotlin/day19/Day19.kt
@@ -2,18 +2,11 @@ package day19
 
 import java.lang.IllegalStateException
 
-
-data class RuleMatch(val index1: Int, val index2: Int)
-
 sealed class Rule(val index: Int)
 
 data class Value(val id: Int, val value : String) : Rule(id) 
-data class EitherMatch(val id: Int, val r1 : RuleMatch, val r2: RuleMatch) : Rule(id)
-data class SingleEitherMatch(val id: Int, val v1 : Int, val v2: Int) : Rule(id)
-
-data class TripeMatch(val id: Int, val r1 : RuleMatch, val index3: Int) : Rule(id)
-data class DoubleMatch(val id: Int, val r1 : RuleMatch) : Rule(id)
-data class SingleMatch(val id: Int, val v1: Int)  : Rule(id)
+data class Or(val id: Int, val b1 : List<Int>, val b2: List<Int>) : Rule(id)
+data class And(val id: Int, val ruleIds: List<Int>)  : Rule(id)
 
 typealias Rules = Map<Int, Rule>
 
@@ -26,32 +19,19 @@ fun _matches(s : String, ruleId: Int, rules : Map<Int, Rule>, index: Int) : Pair
     val rule = rules.get(ruleId)
     val res : Pair<Int, Boolean> = when (rule) {
         is Value -> Pair(index+1, s[index] == rule.value[0])
-        is SingleMatch -> _matches(s, rule.v1, rules, index)
-        is DoubleMatch -> {
-                listOf(rule.r1.index1, rule.r1.index2).fold(Pair(index, true), { acc, rId ->
+        is And -> {
+                rule.ruleIds.fold(Pair(index, true), { acc, rId ->
                     val (newIndex, bool) = _matches(s, rId, rules, acc.first)
                     Pair(newIndex, acc.second && bool)
                 })
         }
-        is TripeMatch -> {
-            listOf(rule.r1.index1, rule.r1.index2, rule.index3).fold(Pair(index, true), { acc, rId ->
-                val (newIndex, bool) = _matches(s, rId, rules, acc.first)
-                Pair(newIndex, acc.second && bool)
-            })
-        }
-        is SingleEitherMatch -> {
-            val m1 = _matches(s, rule.v1, rules, index)
-            if (m1.second) m1 else {
-                _matches(s, rule.v2, rules, index)    
-            }
-        }
-        is EitherMatch -> {
-            val m1 = listOf(rule.r1.index1, rule.r1.index2).fold(Pair(index, true), { acc, rId ->
+        is Or -> {
+            val m1 = rule.b1.fold(Pair(index, true), { acc, rId ->
                 val (newIndex, bool) = _matches(s, rId, rules, acc.first)
                 Pair(newIndex, acc.second && bool)
             })
             if (m1.second) m1 else {
-                listOf(rule.r2.index1, rule.r2.index2).fold(Pair(index, true), { acc, rId ->
+                rule.b2.fold(Pair(index, true), { acc, rId ->
                     val (newIndex, bool) = _matches(s, rId, rules, acc.first)
                     Pair(newIndex, acc.second && bool)
                 }) 
--- day03-kotlin/src/test/kotlin/day19/Day19Test.kt
+++ day03-kotlin/src/test/kotlin/day19/Day19Test.kt
@@ -22,21 +22,20 @@ class Day19Test {
             return Value(id.toInt(), value)
         } else if (singleIndex.matches(s)) {
             val (index, rule1) = singleIndex.find(s)!!.destructured
-            return SingleMatch(index.toInt(), rule1.toInt())
+            return And(index.toInt(), listOf(rule1.toInt()))
         } else if (singleEther.matches(s)) {
             val (index, rule1, rule2) = singleEther.find(s)!!.destructured
-            return SingleEitherMatch(index.toInt(), rule1.toInt(), rule2.toInt())
+            return Or(index.toInt(), listOf(rule1.toInt()), listOf(rule2.toInt()))
         } else if (triple.matches(s)) {
             val (index, rule1, rule2, rule3) = triple.find(s)!!.destructured
-            return TripeMatch(index.toInt(), RuleMatch(rule1.toInt(), rule2.toInt()), rule3.toInt())
+            return And(index.toInt(), listOf(rule1.toInt(), rule2.toInt(), rule3.toInt()))
         } else  {
             val match = r.find(s)!!
             val (index, rule1, rule2, rule3, rule4) = match.destructured
-            val r1 = RuleMatch(rule1.toInt(), rule2.toInt())
             if (rule3 == "") {
-                return DoubleMatch(index.toInt(), r1)
+                return And(index.toInt(), listOf(rule1.toInt(), rule2.toInt()))
             } else {
-                return EitherMatch(index.toInt(), r1, RuleMatch(rule3.toInt(), rule4.toInt()))
+                return Or(index.toInt(), listOf(rule1.toInt(), rule2.toInt()), listOf(rule3.toInt(), rule4.toInt()))
             }
         }
     }
