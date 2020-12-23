fcba23fdacd2eaba97de925f8fab5fa2b5445547
Paul Watson <paul.watson@pdwtech.com>
2020-12-23 09:57:54 +0000

    day 19 part2

--- day03-kotlin/src/main/kotlin/day19/Day19.kt
+++ day03-kotlin/src/main/kotlin/day19/Day19.kt
@@ -11,30 +11,36 @@ data class And(val id: Int, val ruleIds: List<Int>)  : Rule(id)
 typealias Rules = Map<Int, Rule>
 
 fun matches(s : String, rules: Rules) : Boolean {
-    val m = _matches(s, 0, rules, 0)
+    val m = _matches(s, 0, rules, 0, false)
     return m.second && s.length == m.first 
 } 
 
-fun _matches(s : String, ruleId: Int, rules : Map<Int, Rule>, index: Int) : Pair<Int, Boolean> {
-    val rule = rules.get(ruleId)
+fun _matches(s : String, ruleId: Int, rules : Map<Int, Rule>, index: Int, mtc: Boolean) : Pair<Int, Boolean> {
+    val rule = rules[ruleId]
+    if (index  > s.length-1) {
+        println("$ruleId $index==${s.length} $s")
+        return Pair(s.length, !mtc) 
+    }
     val res : Pair<Int, Boolean> = when (rule) {
-        is Value -> Pair(index+1, s[index] == rule.value[0])
+        is Value -> Pair(index + 1, s[index] == rule.value[0])
         is And -> {
-                rule.ruleIds.fold(Pair(index, true), { acc, rId ->
-                    val (newIndex, bool) = _matches(s, rId, rules, acc.first)
-                    Pair(newIndex, acc.second && bool)
-                })
+            matchAnd(rule, index, s, rules, mtc)
         }
         is Or -> {
             val m1 = rule.b1.fold(Pair(index, true), { acc, rId ->
-                val (newIndex, bool) = _matches(s, rId, rules, acc.first)
+                val (newIndex, bool) = _matches(s, rId, rules, acc.first, mtc)
                 Pair(newIndex, acc.second && bool)
             })
-            if (m1.second) m1 else {
+            if (m1.second) m1 else if (ruleId == 11) {
                 rule.b2.fold(Pair(index, true), { acc, rId ->
-                    val (newIndex, bool) = _matches(s, rId, rules, acc.first)
+                    val (newIndex, bool) = _matches(s, rId, rules, acc.first, rId == 11 || rId == 42)
                     Pair(newIndex, acc.second && bool)
-                }) 
+                })
+            } else {
+                rule.b2.fold(Pair(index, true), { acc, rId ->
+                    val (newIndex, bool) = _matches(s, rId, rules, acc.first, mtc)
+                    Pair(newIndex, acc.second && bool)
+                })
             }
         }
         else -> throw IllegalStateException("Nope")
@@ -42,4 +48,15 @@ fun _matches(s : String, ruleId: Int, rules : Map<Int, Rule>, index: Int) : Pair
     return res
     
 }
+
+private fun matchAnd(
+    rule: And,
+    index: Int,
+    s: String,
+    rules: Map<Int, Rule>,
+    mtc: Boolean
+): Pair<Int, Boolean> = rule.ruleIds.fold(Pair(index, true), { acc, rId ->
+    val (newIndex, bool) = _matches(s, rId, rules, acc.first, mtc)
+    Pair(newIndex, acc.second && bool)
+})
 
--- day03-kotlin/src/test/kotlin/day19/Day19Test.kt
+++ day03-kotlin/src/test/kotlin/day19/Day19Test.kt
@@ -7,43 +7,33 @@ import kotlin.test.assertTrue
 
 class Day19Test {
     
-    val r = Regex("^(\\d+): (\\d+) (\\d+)(?: \\| (\\d+) (\\d+))?$")
-    val singleEther = Regex("^(\\d+): (\\d+) \\| (\\d+)$")
-    val triple = Regex("^(\\d+): (\\d+) (\\d+) (\\d+)$")
     val singleVal = Regex("(\\d+): \"(\\w+)\"")
-    val singleIndex = Regex("^(\\d+): (\\d+)$")
 
     private fun buildRules(ruleStr: String): Rules = ruleStr.lines().map { toRule(it) }.associateBy { it.index }
 
     fun toRule(s: String) : Rule {
 //        println(s)
-        if (s.contains("\"")) {
+        if (s.contains("|")) {
+            val (ruleId, data) = s.split(":")
+            val (a,b) = data.split("|")
+            return Or(ruleId.toInt(), extractIds(a), extractIds(b))
+        }
+        else if (s.contains("\"")) {
             val (id, value) = singleVal.find(s)!!.destructured
             return Value(id.toInt(), value)
-        } else if (singleIndex.matches(s)) {
-            val (index, rule1) = singleIndex.find(s)!!.destructured
-            return And(index.toInt(), listOf(rule1.toInt()))
-        } else if (singleEther.matches(s)) {
-            val (index, rule1, rule2) = singleEther.find(s)!!.destructured
-            return Or(index.toInt(), listOf(rule1.toInt()), listOf(rule2.toInt()))
-        } else if (triple.matches(s)) {
-            val (index, rule1, rule2, rule3) = triple.find(s)!!.destructured
-            return And(index.toInt(), listOf(rule1.toInt(), rule2.toInt(), rule3.toInt()))
-        } else  {
-            val match = r.find(s)!!
-            val (index, rule1, rule2, rule3, rule4) = match.destructured
-            if (rule3 == "") {
-                return And(index.toInt(), listOf(rule1.toInt(), rule2.toInt()))
-            } else {
-                return Or(index.toInt(), listOf(rule1.toInt(), rule2.toInt()), listOf(rule3.toInt(), rule4.toInt()))
-            }
-        }
+        } else {
+            val (ruleId, data) = s.split(":")
+            return And(ruleId.toInt(), extractIds(data))
+        } 
     }
+
+    private fun extractIds(a: String) : List<Int> = a.trim().split(" ").map { it.toInt() }.toList()
 
     @Test
     fun part1example() {
         val (ruleStr, input) = readInput("/day19/test.txt").split("\n\n")
         val rules = buildRules(ruleStr)
+        println(rules)
         val lines = input.lines()
         assertTrue(matches(lines[0],  rules))
         assertFalse(matches(lines[1], rules))
@@ -63,9 +53,9 @@ class Day19Test {
     @Test
     fun part2() {
         val (ruleStr, input) = readInput("/day19/day19.txt").split("\n\n")
-//        val rules = buildRules(ruleStr.replace("8: 42", "8: 42 | 42 8").replace("11: 42 31", ))
-        
-//        println(count)
-//        8: 42
+        val ruleStr1 = ruleStr.replace("8: 42", "8: 42 | 42 8").replace("11: 42 31", "11: 42 31 | 42 11 31")
+        val rules = buildRules(ruleStr1)
+        val count = input.lines().count { matches(it, rules) }
+        println(count)
     }
 }
