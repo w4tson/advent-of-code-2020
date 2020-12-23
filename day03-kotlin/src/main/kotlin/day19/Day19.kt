23d49397b3f1e84ea2407e800747165dfd15ad93
Paul Watson <paul.watson@pdwtech.com>
2020-12-23 10:25:54 +0000

    Refactor

--- day03-kotlin/src/main/kotlin/day19/Day19.kt
+++ day03-kotlin/src/main/kotlin/day19/Day19.kt
@@ -18,29 +18,23 @@ fun matches(s : String, rules: Rules) : Boolean {
 fun _matches(s : String, ruleId: Int, rules : Map<Int, Rule>, index: Int, mtc: Boolean) : Pair<Int, Boolean> {
     val rule = rules[ruleId]
     if (index  > s.length-1) {
-        println("$ruleId $index==${s.length} $s")
         return Pair(s.length, !mtc) 
     }
     val res : Pair<Int, Boolean> = when (rule) {
         is Value -> Pair(index + 1, s[index] == rule.value[0])
         is And -> {
-            matchAnd(rule, index, s, rules, mtc)
+            matchAnd(rule.ruleIds, index, s, rules, mtc)
         }
         is Or -> {
-            val m1 = rule.b1.fold(Pair(index, true), { acc, rId ->
-                val (newIndex, bool) = _matches(s, rId, rules, acc.first, mtc)
-                Pair(newIndex, acc.second && bool)
-            })
+            val m1 = matchAnd(rule.b1, index, s, rules, mtc)
             if (m1.second) m1 else if (ruleId == 11) {
+                matchAnd(rule.b2, index, s, rules, mtc)
                 rule.b2.fold(Pair(index, true), { acc, rId ->
-                    val (newIndex, bool) = _matches(s, rId, rules, acc.first, rId == 11 || rId == 42)
+                    val (newIndex, bool) = _matches(s, rId, rules, acc.first, rId == 11)
                     Pair(newIndex, acc.second && bool)
                 })
             } else {
-                rule.b2.fold(Pair(index, true), { acc, rId ->
-                    val (newIndex, bool) = _matches(s, rId, rules, acc.first, mtc)
-                    Pair(newIndex, acc.second && bool)
-                })
+                matchAnd(rule.b2, index, s, rules, mtc)
             }
         }
         else -> throw IllegalStateException("Nope")
@@ -50,12 +44,12 @@ fun _matches(s : String, ruleId: Int, rules : Map<Int, Rule>, index: Int, mtc: B
 }
 
 private fun matchAnd(
-    rule: And,
+    ruleIds: List<Int>,
     index: Int,
     s: String,
     rules: Map<Int, Rule>,
     mtc: Boolean
-): Pair<Int, Boolean> = rule.ruleIds.fold(Pair(index, true), { acc, rId ->
+): Pair<Int, Boolean> = ruleIds.fold(Pair(index, true), { acc, rId ->
     val (newIndex, bool) = _matches(s, rId, rules, acc.first, mtc)
     Pair(newIndex, acc.second && bool)
 })
